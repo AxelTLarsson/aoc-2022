c00296d81890013ed913d4a98fd14007fec91d27
AxelTLarsson <mail@axellarsson.nu>
2022-12-01 16:14:50 +0100

    🎄 Solve day 1 part 2

--- day1/day1.go
+++ day1/day1.go
@@ -3,6 +3,7 @@ package day1
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -23,8 +24,10 @@ func Run() {
 	// split by "elf"
 	elves := strings.Split(string(dat), "\n\n")
 
+	// create a "stack" to store top 3 elve's calories
+	var top []int = []int{}
+
 	// elf by elf sum up calories
-	max := 0
 	for _, elf := range elves {
 		calories := strings.Fields(elf)
 		// convert to ints and sum up
@@ -35,9 +38,11 @@ func Run() {
 			sum += calory
 		}
 
-		if sum > max {
-			max = sum
-		}
+		top = append(top, sum)
 	}
-	fmt.Println(max)
+	sort.Sort(sort.Reverse(sort.IntSlice(top)))
+	fmt.Println("top3:", top[:3])
+	sum := top[0] + top[1] + top[2]
+	fmt.Println("sum:", sum)
+
 }
