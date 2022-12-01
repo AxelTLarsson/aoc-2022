0e198011790223fb400128b5e75ba976072277a4
AxelTLarsson <mail@axellarsson.nu>
2022-12-01 13:36:45 +0100

    🎉 Init repo

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Advent of Code 2022";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.go pkgs.gopls ];
+          name = "aoc-2022";
+        };
+      });
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module axeltlarsson/aoc-2022
+
+go 1.19
